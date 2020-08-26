43d7b11fc41f540db2235cd105b93385704428c5
Rebecca Seiron <Rebecca.Seiron@hotmail.com>
2020-08-26 22:52:02 +0200

    first init

--- includes/config/dbconnect.php
+++ includes/config/dbconnect.php
@@ -2,17 +2,12 @@
  error_reporting(-1);
  ini_set("display_errors" , 1);
  
- $servername = "studentmysql.miun.se";
-  $susername = "rese1800";
-  $password = "5d1azfoh";
-  $dbname = "rese1800";
 
-/*
   $servername = "localhost";
   $susername = "root";
   $password = "";
   $dbname = "projektw2";
-*/
+
   // Create connection
   $conn = new mysqli($servername, $susername, $password, $dbname);
   // Check connection
